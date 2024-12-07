61d2aab355882d413d5d9258f99eb4fd84922f21
HudsonAdjetey <adjetey545@gmail.com>
2024-12-07 03:09:47 -0800

    paymment statement

--- server/router/PaymentRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { initPayment } = require("../controller/paymentController");
-const PaymentRouter = express.Router();
-
-PaymentRouter.post("/init-payment", initPayment);
-
-module.exports = PaymentRouter;
